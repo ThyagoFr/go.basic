81e93bd92fbc2c0809b34db0f52d73536046fded
ThyagoFr <thyagofr@alu.ufc.br>
2020-03-12 15:04:10 -0300

    update...

--- indicadores/mediaMovel.go
+++ indicadores/mediaMovel.go
@@ -1,19 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// 10 AMOSTRAS
+	media := []float64{}
 	valores := []float64{100,105,115,130,150,175,205,240,280,325,375,430,490,555,625,700,780,865,955,1050}
 	n := 10
-	valor := 0.0
-	for j := n; j<len(valores); j++ {
-		fmt.Println(j,valores[j])
-		for i:= 0; i<=10; i++{
+	for j := n-1; j<len(valores); j++ {
+		valor := 0.0
+		for i:= 0; i<n; i++{
 			valor += valores[j-i]
 		}
-		valores[j] = valor/float64(n)
+		k := valor/float64(n)
+		media = append(media,k)
 	}
-	fmt.Println(valores)
-	fmt.Println(len(valores))
+	fmt.Println(media)
 }
