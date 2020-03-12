7567a8746493166abf02663d7887bf2c8df5f22d
ThyagoFr <thyagofr@alu.ufc.br>
2020-03-12 17:26:31 -0300

    Update...

--- intermediario/interface/composicao/composicao.go
+++ intermediario/interface/composicao/composicao.go
@@ -1,1 +1,34 @@
-package composicao
+package main
+
+import "fmt"
+
+type esportivo interface {
+	ligarTurbo()
+}
+type luxuoso interface {
+	fazerBaliza()
+}
+
+type esportivoLuxuoso interface {
+	esportivo
+	luxuoso
+}
+
+type bmw7 struct {}
+
+func (b bmw7) fazerBaliza(){
+	fmt.Println("Baliza...")
+}
+
+func (b bmw7) ligarTurbo(){
+	fmt.Println("Turbo...")
+}
+
+func main() {
+	var coisa1 luxuoso = bmw7{}
+	coisa1.fazerBaliza()
+	var coisa2 esportivo = bmw7{}
+	coisa2.ligarTurbo()
+	var coisa3 esportivoLuxuoso = bmw7{}
+	coisa3.fazerBaliza()
+}
--- intermediario/interface/interfaceHeranca/interface.go
+++ intermediario/interface/interfaceHeranca/interface.go
@@ -1,1 +1,44 @@
-package interfaceHeranca
+package main
+
+import "fmt"
+
+type imprimivel interface {
+	toString() string
+}
+
+type pessoa struct {
+	nome string
+	sobrenome string
+}
+
+type produto struct {
+	valor float64
+	nome string
+}
+
+func (p produto) toString() string {
+	return fmt.Sprintf("%s - %.2f", p.nome, p.valor)
+}
+
+func (p pessoa) toString() string {
+	return p.nome + " " + p.sobrenome
+}
+
+func imprimir(i imprimivel){
+	fmt.Println(i.toString())
+}
+
+func main() {
+	// Implementações de interface sao IMPLICITAS em GO, produto e pessoa implementam,nesse caso, a mesma interface (imprimivel)
+	// Dessa forma, imprimivel consegue encapsular qualquer struct que implementa os metodos da interface
+	// porem obedecendo o conceito de polimorfismo, de forma que produto e pessoa tem seus proprios "jeitos" quando se é chamada o toString()
+
+	var qualquercoisa imprimivel = produto{nome : "PS4", valor: 2000}
+	fmt.Println(qualquercoisa.toString())
+	qualquercoisa = pessoa{"Thyago", "Freitas"}
+	fmt.Println(qualquercoisa.toString())
+	fmt.Println("/////////////////////////////////////")
+
+	pessoaX := pessoa{"Thyago", "Silva"}
+	imprimir(pessoaX)
+}
